2294f2066687e3387be0dbbae55ec4ed69764b13
Mark Hähnel <hello@markhaehnel.de>
2019-01-24 16:38:56 +0100

    Just plain old js bro

---
+++ src/frontend/js/main.js
@@ -0,0 +1,20 @@
+let ts3clients = document.querySelector('#ts3clients')
+
+function handleError() {
+  ts3clients.textContent = "Offline"
+}
+
+(async function(){
+  try {
+    let response = await fetch('/api/teamspeak')
+    if (response.status === 200) {
+      let data = response.json()
+      
+      ts3clients.textContent = data.clientsOnline + '/' + data.clientsMax
+    } else {
+      handleError()
+    }
+  } catch (e) {
+    handleError()
+  }
+})();
